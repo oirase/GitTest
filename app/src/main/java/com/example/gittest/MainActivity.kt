22695a75a90ad06167408c771aa61e971e6356b9
oirase <mabovermicelli@outlook.com>
2022-06-01 15:52:22 +0900

    1

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        const a = 1
+        val a = 1
     }
 }
